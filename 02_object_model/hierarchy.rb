2398b743f3fe0e5a5e2e860f13e2fb7537cbe63d
Yuko Yuri <y.yuri@everyleaf.com>
2022-01-20 19:49:47 +0900

    回答

--- 02_object_model/hierarchy.rb
+++ 02_object_model/hierarchy.rb
@@ -30,6 +30,7 @@ end
 # - C1.ancestors.first(2) が [C1, M1] となる
 # - C1.new.name が 'C1' を返す
 class C1
+  include M1
   def name
     'C1'
   end
@@ -41,6 +42,7 @@ end
 # - C2.ancestors.first(2) が [M1, C2] となる
 # - C2.new.name が 'M1' を返す
 class C2
+  prepend M1
   def name
     'C2'
   end
@@ -51,14 +53,20 @@ end
 # 次の動作をする C3 class, MySuperClass class を実装する
 # - C3.ancestors.first(6) が [M1, C3, M2, M3, MySuperClass, M4] となる
 # - C3.new.name が 'M1' を返す
-class C3
+class MySuperClass
+  prepend M3
+  prepend M2
+  include M4
+end
+class C3 < MySuperClass
+  prepend M1
   def name
     'C3'
   end
 end
 
 
-# Q4.
+# Q4.　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
 # 次の動作をする C4 class のメソッド increment を実装する
 # - increment メソッドを呼ぶと value が +1 される
 # - また、increment メソッドは value を文字列にしたものを返す
@@ -68,6 +76,16 @@ end
 #   c4.increment # => "3"
 # - 定義済みのメソッド (value, value=) は private のままとなっている
 class C4
+  # 特異クラスのインスタンス変数にセットしてあげないといけなさそう
+
+  def increment
+    unless value
+      self.value = 0
+    end
+
+    self.value = (value.to_i + 1).to_s
+  end
+
   private
 
   attr_accessor :value
@@ -80,6 +98,11 @@ end
 # - C5.new.another_name が文字列 "M1" を返す
 # - C5.new.other_name が文字列 "Refined M1" を返す
 module M1Refinements
+  refine M1 do
+    def name
+      "Refined M1"
+    end
+  end
 end
 
 class C5
@@ -104,4 +127,8 @@ end
 class C6
   include M1
   using M1Refinements
+
+  def name
+    super
+  end
 end
