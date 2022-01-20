bf0ddd40d0f4e2d40b5fb14a997a23082d75f717
Yuko Yuri <y.yuri@everyleaf.com>
2022-01-20 19:58:54 +0900

    回答した

--- 02_object_model/hoge_everywhere.rb
+++ 02_object_model/hoge_everywhere.rb
@@ -6,3 +6,33 @@
 # - Class
 # - Hash
 # - TrueClass
+
+class String
+  def hoge
+    'hoge'
+  end
+end
+
+class Numeric
+  def hoge
+    'hoge'
+  end
+end
+
+class Class
+  def hoge
+    'hoge'
+  end
+end
+
+class Hash
+  def hoge
+    'hoge'
+  end
+end
+
+class TrueClass
+  def hoge
+    'hoge'
+  end
+end
